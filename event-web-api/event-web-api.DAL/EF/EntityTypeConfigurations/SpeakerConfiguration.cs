78eb435eeced97244f2ff0965646737c86d7912b
Vladislav Zhogol <vzhogol176@gmail.com>
2023-11-01 12:32:20 +0300

    Create ef and configurations for models

---
+++ event-web-api/event-web-api.DAL/EF/EntityTypeConfigurations/SpeakerConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using event_web_api.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace event_web_api.DAL.EF.EntityTypeConfigurations
+{
+    public class SpeakerConfiguration : IEntityTypeConfiguration<Speaker>
+    {
+        public void Configure(EntityTypeBuilder<Speaker> builder)
+        {
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.Id).ValueGeneratedOnAdd();
+            builder.Property(s => s.FirstName).IsRequired();
+            builder.Property(s => s.LastName).IsRequired();
+        }
+    }
+}
