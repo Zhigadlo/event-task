2de02cadffb09dfba40e442ae1f4b9e0d02e2a08
Vladislav Zhogol <vzhogol176@gmail.com>
2023-11-01 21:45:55 +0300

    Add validators for main entities

---
+++ event-web-api/event-web-api.BLL/Validation/SpeakerValidator.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using FluentValidation;
+
+namespace event_web_api.BLL.Validation
+{
+    public class SpeakerValidator : AbstractValidator<Speaker>
+    {
+        public SpeakerValidator()
+        {
+            RuleFor(s => s.FirstName).NotEmpty().WithMessage("First name can't be empty");
+            RuleFor(s => s.LastName).NotEmpty().WithMessage("Last name can't be empty");
+        }
+    }
+}
