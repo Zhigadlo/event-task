7cc6ff3bd56e5528e9d4470acea706e4dec8f137
Vladislav Zhogol <vzhogol176@gmail.com>
2023-11-01 21:46:09 +0300

    Create custom exceptions

---
+++ event-web-api/Entities/Exceptions/EntityIsNotValidException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions
+{
+    public class EntityIsNotValidException : Exception
+    {
+        public EntityIsNotValidException(string? message) : base(message)
+        {
+        }
+    }
+}
