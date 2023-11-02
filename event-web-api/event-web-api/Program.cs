c92393fb12486440f8cdc160dd0caac195934bed
Vladislav Zhogol <vzhogol176@gmail.com>
2023-11-02 15:30:12 +0300

    DI setting up

--- event-web-api/event-web-api/Program.cs
+++ event-web-api/event-web-api/Program.cs
@@ -1,4 +1,18 @@
+using Contracts.Repositories;
+using Contracts.Services;
+using event_web_api.BLL.Managers;
+using event_web_api.DAL.EF;
+using event_web_api.DAL.Managers;
+using event_web_api.Middleware;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<EventContext>(options =>
+                             options.UseSqlServer(builder.Configuration.GetConnectionString("EventsDatabase")));
+
+builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
+builder.Services.AddScoped<IServiceManager, ServiceManager>();
 
 builder.Services.AddControllers();
 
@@ -12,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
